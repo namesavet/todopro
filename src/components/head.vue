06fd30a11e5cbd7aa6fdb8692396556541b90353
Pongsavet <Pongsavet_name@hotmail.com>
2021-06-22 00:58:03 +0700

    headerindex

--- src/components/head.vue
+++ src/components/head.vue
@@ -1,36 +1,57 @@
 <template>
-<div class="row">
-  
-    <div class="col">
-
-     <div class="borderdash">    
-
-            <div class="profileImg q-ma-md" >
-
-          </div>
-
+  <div class="row q-pa-md">
+    <div class="col-4">
+      <div class="borderdash row justify-center items-center">
+        <div class="profileImg ">
+              <q-img
+      src="../image/profile.png"
+      
+      style="height: 100%; max-width: 100% ;border-radius: 20%;"
+    />
         </div>
-
+      </div>
     </div>
 
-</div>
-
-
+    <div class="col q-ml-md q-mt-sm q-gutter-xs" >
+     
+        <div class="row justify-between">
+          <div class="text-white text-bold" style="font-size:24px">Hello!</div>
+            <q-btn rounded color="green" label="1/2021" style="height:33px" />
+        </div>
+        <div class="row">
+          <div class="text-white text-bold" style="font-size:20px">
+            Pongsavet
+          </div>
+        </div>
+        <div class="row justify-end">
+         <q-btn
+  
+          style="font-size:12px;height:28px"
+          rounded
+          color="red"
+          label="Grade summary"
+        />
+        </div>
+      
+    </div>
+    
+  </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style scoped>
-.profileImg{
- width: 100px;
- height: 100px;
- background: white;
- border-radius: 20%;
+.profileImg {
+  width: 90px;
+  height: 90px;
+  background: white;
+  border-radius: 20%;
 }
-.borderdash{
-    border:dashed gray;
-    border-radius: 20%;
+.borderdash {
+  width: 110px;
+  height: 110px;
+  border: dashed gray 2px;
+  border-radius: 20%;
 }
+
 </style>
