12d901d2393831cfe58d2106862a3eaed5cc6825
konlawat <konlawateiei@gmail.com>
2021-06-22 19:25:23 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-   HEAD
+ 
       { 
         path: '', component: ()  => import('pages/Index.vue') ,
         path: '/Subject', component: ()  => import('pages/Subject.vue') ,
