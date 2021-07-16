7bd1724fd684a83af193e3ac632870700575b956
Ratchaya <ratchaya166@gmail.com>
2021-07-16 22:56:19 +0700

    create page createaccount4.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,28 +4,29 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      {path: '', component: ()  => import('pages/Index.vue') },
-      {path: 'subject', component: ()  => import('pages/Subject.vue')},
-      {path: 'addsubject', component: () => import('pages/Addsubject.vue')},
-      {path: 'course', component: ()  => import('pages/Course.vue')},
-      {path: 'addsubject', component: () => import('pages/Addsubject.vue')},
-      {path: 'backtohome', component: () => import('pages/Index.vue')},
-      {path: 'backtosubject', component: () => import('pages/Subject.vue')},
-      {path: 'gotosubject', component: () => import('pages/Subject.vue')},
-      {path: 'gotoaddsubject', component: () => import('pages/Addsubject.vue')},
-      {path: 'Begin', component: () => import('pages/Begin.vue')},
-      {path: 'Welcome', component: () => import('pages/Welcome.vue')},
-      {path: 'Createaccount1', component: () => import('pages/Createaccount1.vue')},
-      {path: 'Createaccount2', component: () => import('pages/Createaccount2.vue')},
-      {path: 'Createaccount3', component: () => import('pages/Createaccount3.vue')},
-      {path: 'gototestandscore', component: () => import('pages/Testandscore.vue')},
-      
-      
-    
-     ]
-    
-    },
-  
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'subject', component: () => import('pages/Subject.vue') },
+      { path: 'addsubject', component: () => import('pages/Addsubject.vue') },
+
+      { path: 'addsubject', component: () => import('pages/Addsubject.vue') },
+      { path: 'backtohome', component: () => import('pages/Index.vue') },
+      { path: 'backtosubject', component: () => import('pages/Subject.vue') },
+      { path: 'gotosubject', component: () => import('pages/Subject.vue') },
+      { path: 'gotoaddsubject', component: () => import('pages/Addsubject.vue') },
+      { path: 'Begin', component: () => import('pages/Begin.vue') },
+      { path: 'Welcome', component: () => import('pages/Welcome.vue') },
+      { path: 'Createaccount1', component: () => import('pages/Createaccount1.vue') },
+      { path: 'Createaccount2', component: () => import('pages/Createaccount2.vue') },
+      { path: 'Createaccount3', component: () => import('pages/Createaccount3.vue') },
+      { path: 'Createaccount4', component: () => import('pages/Createaccount4.vue') },
+      { path: 'gototestandscore', component: () => import('pages/Testandscore.vue') },
+
+
+
+    ]
+
+  },
+
 
 
 
