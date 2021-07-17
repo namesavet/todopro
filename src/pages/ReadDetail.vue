b8b9b5f14b8efdd12cbab525577521babf193b4f
konlawat <konlawateiei@gmail.com>
2021-07-18 02:29:32 +0700

    Create ReadDetail.vue

---
+++ src/pages/ReadDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="readabook">
+    <q-toolbar>
+      <q-toolbar-title>
+        <router-link to="backtohome">
+          <q-btn flat color="" icon="keyboard_arrow_left" label="Back" style="font-size:16px ;color:#96A7AF" />
+        </router-link>
+      </q-toolbar-title>
+        <q-btn flat round dense  icon="search" class="q-mr-xs" style="color:#96A7AF"/>
+    </q-toolbar>
+
+    </q-page>
+</template>
