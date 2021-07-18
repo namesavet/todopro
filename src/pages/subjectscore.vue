9cbdefc0776f349e1c2b0f0f1d534457bfe4c55e
Pongsavet <Pongsavet_name@hotmail.com>
2021-07-18 14:14:49 +0700

    add warring score

--- src/pages/subjectscore.vue
+++ src/pages/subjectscore.vue
@@ -12,6 +12,9 @@
           />
         </router-link>
       </q-toolbar-title>
+      <div class="settingbtn">
+        <q-btn flat round dense text-color="white" icon="settings" class="" />
+      </div>
     </q-toolbar>
 
     <div class="col q-ml-md q-mt-sm q-gutter-xs">
@@ -47,71 +50,108 @@
       </div>
     </div>
 
-    <div class="row justify-center">
-      <div class="score">
-        <div
-          class="q-my-md q-ml-lg text-white text-bold"
-          style="font-size: 16px"
-        >
-          Midterm exam
-        </div>
 
-        <div class="row justify-center">
-          <q-linear-progress
-            dark
-            stripe
-            rounded
-            size="16px"
-            :value="progress2"
-            color="red"
-            class=""
-            style="width: 280px"
-          />
-        </div>
-      </div>
-      <div class="score">
-        <div
-          class="q-my-md q-ml-lg text-white text-bold"
-          style="font-size: 16px"
-        >
-          test1
-        </div>
+   <div class="row justify-center">
+   
+      <div class="warringscore q-my-md text-white ">
+          <div class="q-my-sm">
 
-        <div class="row justify-center">
-          <q-linear-progress
-            dark
-            stripe
-            rounded
-            size="16px"
-            :value="progress2"
-            color="red"
-            class=""
-            style="width: 280px"
-          />
-        </div>
-      </div>
-      <div class="score">
-        <div
-          class="q-my-md q-ml-lg text-white text-bold"
-          style="font-size: 16px"
-        >
-          Homework1
-        </div>
+              <div class="row justify-center">
+            You must score 22 points to pass a E
+              </div>
+             <div class="row justify-center">
+            grade and 62 points to A grade
+              </div>
 
-        <div class="row justify-center">
-          <q-linear-progress
-            dark
-            stripe
-            rounded
-            size="16px"
-            :value="progress2"
-            color="red"
-            class=""
-            style="width: 280px"
-          />
-        </div>
-      </div>
+         </div>
+       
+       </div>
+
+        
+   </div>
+    
+     <div class="row justify-center">
+      <div class="scoregrade"></div>
     </div>
+
+
+
+
+
+
+      <div class="row justify-center">
+        <div class="score">
+          <div
+            class="q-my-md q-ml-lg text-white text-bold"
+            style="font-size: 16px"
+          >
+            Midterm exam
+          </div>
+
+          <div class="row justify-center">
+            <q-linear-progress
+              dark
+              stripe
+              rounded
+              size="16px"
+              :value="progress2"
+              color="red"
+              class=""
+              style="width: 280px"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="row justify-center">
+        <div class="score">
+          <div
+            class="q-my-md q-ml-lg text-white text-bold"
+            style="font-size: 16px"
+          >
+            test1
+          </div>
+
+          <div class="row justify-center">
+            <q-linear-progress
+              dark
+              stripe
+              rounded
+              size="16px"
+              :value="progress2"
+              color="red"
+              class=""
+              style="width: 280px"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="row justify-center">
+        <div class="score">
+          <div
+            class="q-my-md q-ml-lg text-white text-bold"
+            style="font-size: 16px"
+          >
+            Homework1
+          </div>
+
+          <div class="row justify-center">
+            <q-linear-progress
+              dark
+              stripe
+              rounded
+              size="16px"
+              :value="progress2"
+              color="red"
+              class=""
+              style="width: 280px"
+            />
+          </div>
+        </div>
+      </div>
+   <br>
+<br>
+<br>
+<br>
 
     <q-footer elevated>
       <q-toolbar
@@ -136,7 +176,14 @@
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    return {
+      progress1: 0.4,
+      progress2: 0.62,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -150,4 +197,26 @@ export default {};
   border-radius: 25px;
   margin-top: 10px;
 }
+.settingbtn {
+  width: 35px;
+  height: 34px;
+  background: #40df9f;
+  border-radius: 15%;
+}
+.warringscore{
+    width: 317px;
+  height: 55px;
+  background: #FF5656;
+  border-radius: 25px;
+  margin-top: 10px;
+}
+.scoregrade{
+  width: 271px;
+  height: 268px;
+  background: #2a3c44;
+  border-radius: 25px;
+}
 </style >
+
+<script>
+</script>
