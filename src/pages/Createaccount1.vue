b0940a8194f4256d18b0d82a6a667ce2ab37bd5a
Ratchaya <ratchaya166@gmail.com>
2021-07-23 17:02:14 +0700

    edit variable name

--- src/pages/Createaccount1.vue
+++ src/pages/Createaccount1.vue
@@ -5,16 +5,16 @@
       <div class="texttitle1 q-ml-lg q-mt-lg">Hello !</div>
       <div class="texttitle2 q-ml-lg q-mt-sm">Lets introduce</div>
 
-      <div class="row justify-center items-center q-ml-lg q-mt-xl">
-        <div class="icon-name" style="overflow: hidden">
+      <div class="row justify-center items-center q-mt-xl">
+        <div class="icon_Profile" style="overflow: hidden">
           <div class="row items-center justify-center q-mt-md">
             <q-img src="../image/people.png" style="max-width: 45%" />
           </div>
         </div>
-        <div class="enter-name col-9 q-ml-md q-gutter-xs">
+        <div class="col-9 q-ml-md">
           <q-input
             :input-style="{ color: 'white' }"
-            v-model="your_fullname"
+            v-model="Fullname"
             label-color="grey"
             label="Your full name"
             color="white"
@@ -25,16 +25,16 @@
         </div>
       </div>
 
-      <div class="row justify-center items-center q-ml-lg q-mt-lg">
-        <div class="icon-name" style="overflow: hidden">
+      <div class="row justify-center items-center q-mt-sm">
+        <div class="icon_Profile" style="overflow: hidden">
           <div class="row items-center justify-center q-mt-md">
             <q-img src="../image/people.png" style="max-width: 45%" />
           </div>
         </div>
-        <div class="enter-name col-9 q-ml-md q-gutter-xs">
+        <div class="col-9 q-ml-md">
           <q-input
             :input-style="{ color: 'white' }"
-            v-model="username"
+            v-model="Username"
             label-color="grey"
             label="Username"
             color="white"
@@ -46,9 +46,9 @@
       </div>
 
       <div class="row justify-center items-center">
-        <div class="button-Sign_in q-mt-lg">
+        <div class="button_back q-mt-lg">
           <q-btn
-           @click="$router.push({name:'Welcome'})"
+            @click="$router.push({ name: 'Welcome' })"
             push
             align="center"
             no-caps
@@ -57,7 +57,7 @@
             style="width: 50px; background: #286053; color: #3dd598"
           />
         </div>
-        <div class="button-Sign_in q-mt-lg q-ml-md">
+        <div class="button-next q-mt-lg q-ml-md">
           <q-btn
             @click="gotocreateaccount2()"
             push
@@ -66,7 +66,7 @@
             label="Next "
             icon-right="arrow_forward"
             size="20px"
-            style="width: 200px; background: #40df9f; color: white"
+            style="width: 250px; background: #40df9f; color: white"
           />
         </div>
       </div>
@@ -78,18 +78,19 @@
 export default {
   data() {
     return {
-      your_fullname: "",
-      username: "",
+      Fullname: "",
+      Username: "",
     };
   },
-  methods:{
-    gotocreateaccount2(){
-      console.log(this.your_fullname);
-    this.$router.push({
-      path:'/Createaccount2'
-    })
-    }
-  }
+  methods: {
+    gotocreateaccount2() {
+      console.log(this.Fullname);
+      console.log(this.Username);
+      this.$router.push({
+        path: "/Createaccount2",
+      });
+    },
+  },
 };
 </script>
 
@@ -98,10 +99,10 @@ export default {
   background: #22343c;
 }
 .Logo {
-  width: 50px;
+  width: 15%;
   height: 50px;
   background: #40df9f;
-  border-radius: 15%;
+  border-radius: 15px;
   margin-top: 100px;
 }
 .texttitle1 {
@@ -115,12 +116,10 @@ export default {
   color: #96a7af;
 }
 
-.icon-name {
+.icon_Profile {
   width: 38px;
   height: 48px;
   background: #625b39;
   border-radius: 15%;
 }
-
-
 </style>
--- src/pages/Createaccount2.vue
+++ src/pages/Createaccount2.vue
@@ -5,59 +5,89 @@
       <div class="texttitle1 q-ml-lg q-mt-lg">Hello !</div>
       <div class="texttitle2 q-ml-lg q-mt-sm">Ratchaya Narueradanan</div>
 
-      <div class="row justify-center items-center q-ml-lg q-mt-xl">
-        <div class="icon-password" style="overflow: hidden">
+      <div class="row justify-center items-center q-mt-xl">
+        <div class="icon_Key" style="overflow: hidden">
           <div class="row items-center justify-center q-mt-md">
             <q-img src="../image/key.png" style="max-width: 45%" />
           </div>
         </div>
-        <div class="enter-name col-9 q-ml-md q-gutter-xs">
+        <div class="col-9 q-ml-md">
           <q-input
-            v-model="create_password"
+            :input-style="{ color: 'white' }"
+            v-model="Password"
             label-color="grey"
             label="Create password"
             color="white"
-          />
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please enter password ',
+            ]"
+            :type="isPwd ? 'Password' : 'text'"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
         </div>
       </div>
 
-      <div class="row justify-center items-center q-ml-lg q-mt-lg">
-        <div class="icon-password" style="overflow: hidden">
+      <div class="row justify-center items-center q-mt-sm">
+        <div class="icon_Key" style="overflow: hidden">
           <div class="row items-center justify-center q-mt-md">
             <q-img src="../image/key.png" style="max-width: 45%" />
           </div>
         </div>
-        <div class="enter-name col-9 q-ml-md q-gutter-xs">
+        <div class="col-9 q-ml-md">
           <q-input
-            v-model="repeat_password"
+            :input-style="{ color: 'white' }"
+            v-model="Password"
             label-color="grey"
             label="Repeat your password"
             color="white"
-          />
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please enter password ',
+            ]"
+            :type="isPwd ? 'Password' : 'text'"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
         </div>
       </div>
 
-      <div class="row justify-center items-center">
-        <div class="button-back q-mt-lg">
-          <q-btn
-            push
-            align="center"
-            no-caps
-            icon-right="arrow_back"
-            size="20px"
-            style="width: 50px; background: #286053; color: #3dd598"
-          />
-        </div>
-        <div class="button-next q-mt-lg q-ml-md">
-          <q-btn
-            push
-            align="center"
-            no-caps
-            label="Next "
-            icon-right="arrow_forward"
-            size="20px"
-            style="width: 250px; background: #40df9f; color: white"
-          />
+      <div>
+        <div class="row justify-center items-center">
+          <div class="button_back q-mt-lg">
+            <q-btn
+              @click="$router.push({ name: 'Createaccount1' })"
+              push
+              align="center"
+              no-caps
+              icon-right="arrow_back"
+              size="20px"
+              style="width: 50px; background: #286053; color: #3dd598"
+            />
+          </div>
+          <div class="button-next q-mt-lg q-ml-md">
+            <q-btn
+              @click="gotocreateaccount3()"
+              push
+              align="center"
+              no-caps
+              label="Next "
+              icon-right="arrow_forward"
+              size="20px"
+              style="width: 250px; background: #40df9f; color: white"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -68,9 +98,19 @@
 export default {
   data() {
     return {
-      create_password: "",
-      repeat_password: "",
+      Password: "",
+
+      isPwd: true,
     };
+  },
+  methods: {
+    gotocreateaccount3() {
+      console.log(this.Password);
+
+      this.$router.push({
+        path: "/Createaccount3",
+      });
+    },
   },
 };
 </script>
@@ -80,10 +120,10 @@ export default {
   background: #22343c;
 }
 .Logo {
-  width: 50px;
+  width: 15%;
   height: 50px;
   background: #40df9f;
-  border-radius: 15%;
+  border-radius: 15px;
   margin-top: 100px;
 }
 .texttitle1 {
@@ -97,7 +137,7 @@ export default {
   color: #96a7af;
 }
 
-.icon-password {
+.icon_Key {
   width: 38px;
   height: 48px;
   background: #623a42;
