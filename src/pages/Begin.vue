32187f04cb347bf7864c425cf0939de33592cfda
Ratchaya <ratchaya166@gmail.com>
2021-07-23 16:57:13 +0700

    edit begin.vue at class "texttitle1" and "texttitle2"

--- src/pages/Begin.vue
+++ src/pages/Begin.vue
@@ -1,28 +1,20 @@
 <template>
-  <q-page 
-  @click="$router.push({name:'Welcome'})"
-  class="Begin q-pt-xl">
-
+  <q-page @click="$router.push({ name: 'Welcome' })" class="Begin q-pt-xl">
     <div class="col">
-      <div class="texttitle1 q-ml-md">Note School</div>
+      <div class="texttitle1 q-ml-md">Study Planner</div>
       <div class="texttitle2 q-ml-md q-mt-md">
-        <div class="row2-1">Easy to remember with the</div>
-        <div class="row2-2">school notes app</div>
+        <div class="row2-1">Easy to remember with</div>
+        <div class="row2-2">the school notes app</div>
       </div>
       <div class="texttitle1 q-ml-md q-mt-md">Let us help !!!</div>
       <div class="texttitle2 q-ml-xl q-mt-md">
-        <div class="row">-remember work</div>
-        <div class="row">-remember the score</div>
-        <div class="row">-remember to read</div>
-        <div class="row">-remember activities</div>
+        <div class="row">-Remember work</div>
+        <div class="row">-Remember the score</div>
+        <div class="row">-Remember to read</div>
+        <div class="row">-Remember activities</div>
       </div>
 
-      <div
-        class="read-icon absolute-bottom-right q-mr-md justify-center"
-        style="overflow: hidden"
-      >
-        <q-img src="../image/read.png" style="height: 100%; max-width: 80%" />
-      </div>
+      <div class="read-icon absolute-bottom-right" ></div>
     </div>
   </q-page>
 </template>
@@ -49,9 +41,9 @@ export default {};
 }
 
 .read-icon {
-  width: 150px;
+  width: 160px;
   height: 210px;
-  background: #ff464f;
-  border-radius: 15%;
+  background: url("../image/read.png");
+  background-repeat: no-repeat;
 }
 </style>
