bc9a5c15026a9281c584f98e31059738f3f848f3
konlawat <konlawateiei@gmail.com>
2021-07-31 22:06:23 +0700

    Update profile.vue

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -55,7 +55,7 @@
                     </div>
                 </div>
 
-                <div class="row ">
+                <div class="row " @click="$router.push({ name: 'Begin' })" push>
                     <div class="q-ml-md">
                         <q-avatar icon="logout" style="color:#96A7AF"/>
                     </div>
