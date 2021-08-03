3ebeb11a145c8df1b9b07123ecd4b31c13a0c343
Ratchaya <ratchaya166@gmail.com>
2021-08-04 00:37:17 +0700

    delete path subjectchapter

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,7 +14,7 @@ const routes = [
       { path: 'Createaccount4', name: 'Createaccount4', component: () => import('pages/Createaccount4.vue') },
       { path: 'Forgotpassword', name: 'Forgotpassword', component: () => import('pages/Forgotpassword.vue') },
       { path: 'Resetpassword', name: 'Resetpassword', component: () => import('pages/Resetpassword.vue') },
-      { path: 'Newpassword', name: 'Newpassword', component: () => import('pages/Newpassword.vue') }, 
+      { path: 'Newpassword', name: 'Newpassword', component: () => import('pages/Newpassword.vue') },
       { path: 'profile', name: 'profile', component: () => import('pages/profile.vue') },
       { path: 'profileDetail', name: 'profileDetail', component: () => import('pages/profileDetail.vue') },
       { path: 'grade summary', name: 'grade summary', component: () => import('pages/grade summary.vue') },
@@ -32,21 +32,21 @@ const routes = [
       { path: 'addcalendar', name: 'addcalendar', component: () => import('pages/addcalendar.vue') },
       { path: 'event', name: 'event', component: () => import('pages/event.vue') },
       { path: 'editevent', name: 'editevent', component: () => import('pages/editevent.vue') },
-{ path: 'editsubjectchapter', name: 'editsubjectchapter', component: () => import('pages/editsubjectchapter.vue') },
-{ path: 'subjectchapter', name: 'subjectchapter', component: () => import('pages/subjectchapter.vue') },
+      { path: 'editsubjectchapter', name: 'editsubjectchapter', component: () => import('pages/editsubjectchapter.vue') },
+      
 
 
-     ]
-    
-    },
+    ]
 
- 
+  },
+
+
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '*',
     component: () => import('pages/Error404.vue')
   }
- ]
+]
 
 export default routes
