85a670c70a081fc5c7f8347eb68b93b88721b1e5
Ratchaya <ratchaya166@gmail.com>
2021-10-12 00:32:33 +0700

    Edit account creation function
    Add page Createaccount4 and  Createaccount5

--- src/pages/Createaccount3.vue
+++ src/pages/Createaccount3.vue
@@ -2,34 +2,57 @@
   <q-page class="addbackground q-pt-xl">
     <div class="col">
       <div class="Logo q-ml-lg"></div>
-      <div class="texttitle1 q-ml-lg q-mt-lg">Sign up</div>
-      <div class="texttitle2 q-ml-lg q-mt-sm">to start working</div>
+      <div class="texttitle1 q-ml-lg q-mt-lg">Hello !</div>
+      <div class="texttitle2 q-ml-lg q-mt-sm">Lets introduce</div>
 
       <div class="row justify-center items-center q-mt-md">
-        <div class="col-10 ">
+        <div class="icon_Profile" style="overflow: hidden">
+          <div class="row items-center justify-center q-mt-sm">
+            <q-icon name="credit_card" size="30px" style="color: #ffc542" />
+          </div>
+        </div>
+        <div class="col-9 q-ml-md q-gutter-xs">
           <q-input
-          
-            square
+            class="q-mt-md"
             :input-style="{ color: 'white' }"
+            v-model="StudentID"
             label-color="grey"
-            label="Telephone number"
+            label="StudentID"
             color="white"
-            v-model="Phone"
-            mask="### - ### - ####"
-            style="font-size: 15px"
             :rules="[
-              (val) =>
-                (val && val.length > 0) || 'Please enter telephone number',
+              (val) => (val && val.length > 0) || 'Please enter studentID',
             ]"
-          >
-            <template v-slot:before>
-              <q-icon name="call" color="white" />
-            </template>
-          </q-input>
+          />
         </div>
       </div>
 
-      <div class="row justify-center items-center ">
+      <div class="row justify-center items-center">
+        <div class="icon_Profile" style="overflow: hidden">
+          <div class="row items-center justify-center q-mt-sm">
+            <q-icon name="school" size="30px" style="color: #ffc542" />
+          </div>
+        </div>
+        <div class="col-9 q-ml-md q-gutter-xs">
+          <q-input
+            class="q-mt-md"
+            :input-style="{ color: 'white' }"
+            v-model="Major"
+            label-color="grey"
+            label="Major"
+            color="white"
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please enter major ',
+            ]"
+          />
+        </div>
+      </div>
+
+        
+
+       
+      
+
+      <div class="row justify-center items-center">
         <div class="button_back q-mt-lg">
           <q-btn
             @click="$router.push({ name: 'Createaccount2' })"
@@ -62,14 +85,14 @@
 export default {
   data() {
     return {
-      Phone: "",
+      StudentID: "",
+      Major: "",
     };
   },
-
   methods: {
     gotocreateaccount4() {
-      console.log(this.Phone);
-
+      console.log(this.StudentID);
+      console.log(this.Major);
       this.$router.push({
         path: "/Createaccount4",
       });
@@ -79,5 +102,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
