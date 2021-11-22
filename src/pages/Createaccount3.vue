e847d60734d1bc5f3284b2f9cdd2432748503ce6
Ratchaya <ratchaya166@gmail.com>
2021-11-22 19:10:38 +0700

    edit q-input

--- src/pages/Createaccount3.vue
+++ src/pages/Createaccount3.vue
@@ -3,47 +3,50 @@
     <div class="col">
       <div class="Logo q-ml-lg"></div>
       <div class="texttitle2 q-ml-lg q-mt-sm">Semester</div>
-      <div class="row justify-center items-center">
-        <div class="icon_Profile" style="overflow: hidden">
-          <div class="row items-center justify-center q-mt-sm">
-            <q-icon
-              name="cast_for_education"
-              size="30px"
-              style="color: #ffc542"
+      <form @submit.prevent="onSubmit">
+        <div class="row justify-center items-center">
+          <div class="icon_Profile" style="overflow: hidden">
+            <div class="row items-center justify-center q-mt-sm">
+              <q-icon
+                name="cast_for_education"
+                size="30px"
+                style="color: #ffc542"
+              />
+            </div>
+          </div>
+          <div class="col-9 q-ml-md q-gutter-xs">
+            <q-input
+              required
+              class="q-mt-md"
+              :input-style="{ color: 'white' }"
+              v-model="Semester_name"
+              label-color="grey"
+              label="1/2020"
+              mask="#/####"
+              color="white"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please enter Semester ',
+              ]"
             />
           </div>
         </div>
-        <div class="col-9 q-ml-md q-gutter-xs">
-          <q-input
-            class="q-mt-md"
-            :input-style="{ color: 'white' }"
-            v-model="Semester_name"
-            label-color="grey"
-            label="1/2020"
-            mask="#/####"
-            color="white"
-            :rules="[
-              (val) => (val && val.length > 0) || 'Please enter Semester ',
-            ]"
-          />
-        </div>
-      </div>
 
-      <div class="row justify-center items-center">
-   
-        <div class="button-next q-mt-lg q-ml-md">
-          <q-btn
-            @click="onSubmit()"
-            push
-            align="center"
-            no-caps
-            label="Next "
-            icon-right="arrow_forward"
-            size="20px"
-            style="width: 250px; background: #40df9f; color: white"
-          />
+        <div class="row justify-center items-center">
+          <div class="button-next q-mt-lg q-ml-md">
+            <q-btn
+              type="submit"
+              value="onSubmit"
+              push
+              align="center"
+              no-caps
+              label="Next "
+              icon-right="arrow_forward"
+              size="20px"
+              style="width: 250px; background: #40df9f; color: white"
+            />
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   </q-page>
 </template>
@@ -59,7 +62,6 @@ export default {
   },
   methods: {
     onSubmit() {
-      
       axios
         .post("http://localhost:3000/semester/create", {
           uid: this.$route.query.uid,
